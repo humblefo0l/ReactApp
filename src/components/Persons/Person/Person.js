6d97817bd2cd8c5e5c192e412f675eeb23f4ece7
gauravka017 <gauravka017@outlook.com>
2019-10-01 02:03:03 +0530

    Passing unknown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux'
 import classes from './Person.css';
-
+import withClass from '../../../hoc/withClass'
 
 class Person extends Component {
     render(){
@@ -24,4 +24,4 @@ class Person extends Component {
 };
 
 
-export default Person;  
+export default withClass(Person, classes.Person);  
--- src/containers/App.js
+++ src/containers/App.js
@@ -110,7 +110,7 @@ class App extends Component {
         
       </Aux>
 
-    );
+    ); 
   }
 }
 
