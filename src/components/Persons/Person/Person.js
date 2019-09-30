a15c56b72e90e810a0b7e1ce968fb3735c032708
gauravka017 <gauravka017@outlook.com>
2019-10-01 01:36:31 +0530

    Higher Order Component - Introduction

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Aux'
 import classes from './Person.css';
 
 
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
-        return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm a {this.props.name} and i am {this.props.age} year old! </p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
-        
-        )
+        return <Aux>
+                <p key="i1" onClick={this.props.click}>
+                    I'm a {this.props.name} and i am {this.props.age} year old! 
+                </p>
+                <p key="i2">{this.props.children}</p>
+                <input 
+                key="i3"
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name} 
+                />
+        </Aux>;
     }
     
 };
